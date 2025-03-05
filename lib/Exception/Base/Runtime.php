68d3cefefe3c13aed9c4a91a5aa1b7343afb06a4
naingaunglwin-dev <naingaunglwin.wd@gmail.com>
2025-03-05 23:56:05 +0630

    Add Runtime and Type Base Exception Classes

---
+++ lib/Exception/Base/Runtime.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Cubix\Exception\Base;
+
+use RuntimeException;
+
+/**
+ * Base Runtime Exception class
+ */
+class Runtime extends RuntimeException
+{
+    use CubixException;
+}
---
+++ lib/Exception/Base/Type.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Cubix\Exception\Base;
+
+use TypeError;
+
+/**
+ * Base Type Error class
+ */
+class Type extends TypeError
+{
+    use CubixException;
+}
